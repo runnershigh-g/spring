da0e109109bb91be3f18ff74ee205b79c5c43092
bitcamp <bitcamp@DESKTOP-IKN7BB0>
2020-11-06 18:19:49 +0900

    15

---
+++ 15_Spring_JdbcTemplate_Transaction/src/main/java/com/spring/biz/board/impl/BoardServiceImpl.java
@@ -0,0 +1,49 @@
+package com.spring.biz.board.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.biz.board.BoardService;
+import com.spring.biz.board.BoardVO;
+
+@Service("boardService")
+public class BoardServiceImpl implements BoardService{
+	@Autowired
+//	private BoardDAO boardDAO;
+	private BoardDAOSpring boardDAO;
+	
+	public BoardServiceImpl() {
+		System.out.println(">> BoardServiceImpl() 객체 생성");
+	}
+	
+	@Override
+	public void insertBoard(BoardVO vo) {
+		System.out.println(">> BoardServiceImpl.insertBoard() 실행");
+		boardDAO.insertBoard(vo);
+		boardDAO.insertBoard(vo);
+	}
+
+	@Override
+	public void updateBoard(BoardVO vo) {
+		boardDAO.updateBoard(vo);
+	}
+
+	@Override
+	public void deleteBoard(BoardVO vo) {
+		boardDAO.deleteBoard(vo);
+	}
+
+	@Override
+	public BoardVO getBoard(BoardVO vo) {
+		return boardDAO.getBoard(vo);
+		
+	}
+
+	@Override
+	public List<BoardVO> getBoardList(BoardVO vo) {
+		return boardDAO.getBoardList(vo);
+	}
+
+}
